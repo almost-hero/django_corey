db3a14c9c946f7a4068b4fbe8657cdbbcca9e2ba
almost-hero <bgmakar79@gmail.com>
2019-07-05 00:25:10 +0300

    Create authenticate system

---
+++ users/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Profile
+
+class MyFormCreationUser(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = [
+            'username','email','password1','password2'
+        ]
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_qs = User.objects.filter(email = email)
+        if email_qs:
+            raise forms.ValidationError('This email is already exist')
+        return email
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
