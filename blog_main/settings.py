9f41bc98dcde78d315783ad7faed22ec88fe9a78
almost-hero <bgmakar79@gmail.com>
2019-07-13 01:15:54 +0300

    Implement "reset password" functions

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -130,3 +130,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home_page_url'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+# EMAIL_HOST_USER = 'bgmakar79@gmail.com'
+# EMAIL_HOST_PASSWORD = '7772507sS'
